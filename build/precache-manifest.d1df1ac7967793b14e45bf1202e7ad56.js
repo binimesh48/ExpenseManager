98f5d20d2cdb60f41488ab59971fc23f6e5d9600
binimesh48 <49594431+binimesh48@users.noreply.github.com>
2021-07-29 13:40:52 +0530

    initial

---
+++ build/precache-manifest.d1df1ac7967793b14e45bf1202e7ad56.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "bba2c986b213f70ef017",
+    "url": "/static/css/main.70cb1756.chunk.css"
+  },
+  {
+    "revision": "bba2c986b213f70ef017",
+    "url": "/static/js/main.7b6eb8ab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4d0c2629a17b14bb68f2",
+    "url": "/static/css/2.77172d50.chunk.css"
+  },
+  {
+    "revision": "4d0c2629a17b14bb68f2",
+    "url": "/static/js/2.4263a426.chunk.js"
+  },
+  {
+    "revision": "11df31f4b008003723586eff3f5c2233",
+    "url": "/static/media/MONTH_MOBILE.11df31f4.png"
+  },
+  {
+    "revision": "127c2c9c8f4e24f2c7cafdc19a6f0a79",
+    "url": "/static/media/HOME_MOBILE.127c2c9c.png"
+  },
+  {
+    "revision": "ef7b85e5fe1f8ec203cc325b684c96e0",
+    "url": "/static/media/STATISTICS_MOBILE.ef7b85e5.png"
+  },
+  {
+    "revision": "f27a47be6fe96b4eccbc0e84b88cc293",
+    "url": "/static/media/travel.f27a47be.png"
+  },
+  {
+    "revision": "3b7d6f60e2d450b899c322266fc6edfd",
+    "url": "/static/media/man.3b7d6f60.png"
+  },
+  {
+    "revision": "b56b916fd0f0202bbdcce1302d1187c7",
+    "url": "/static/media/Home1.b56b916f.png"
+  },
+  {
+    "revision": "ddc389646cb0f9a80664dad009dc76c8",
+    "url": "/static/media/MONTHLY_NIGHT_MODE.ddc38964.png"
+  },
+  {
+    "revision": "2790e03f631425754918df38a7c1292d",
+    "url": "/static/media/STATISTICS_NIGHT_MODE.2790e03f.png"
+  },
+  {
+    "revision": "b0d0ccbda594449ac792e50046139ef5",
+    "url": "/static/media/SAVINGS_NIGHT.b0d0ccbd.png"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "2d6ce8ef4a0636bce4671c97df2ce94f",
+    "url": "/index.html"
+  }
+];
